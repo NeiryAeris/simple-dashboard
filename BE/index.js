36987e9cb90501220d40b5de09fbdbcda1d5ecbd
FoxVivian <Kukorysenpai3506@gmail.com>
2024-12-05 08:00:33 +0700

    push

--- BE/index.js
+++ BE/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dbConnect from "./dbContext/dbConnect.js";
-
 // Import routes
 import employeeRoutes from "./routes/employeesRouter.js";
 import productRoutes from "./routes/productsRouter.js";
--- BE/routes/booksRouter.js
+++ BE/routes/booksRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getBooks, createBook, updateBook, deleteBook } from "../controllers/bookController.js";
+import multer from "multer";
 
 const router = express.Router();
+const upload = multer({ dest: "uploads/" });
 
 router.get("/allbooks", getBooks);
-router.post("/addbook", createBook);
-router.put("/updatebook/:id", updateBook);
+router.post("/addbook", upload.single("image"), createBook);
+router.put("/updatebook/:id", upload.single("image"), updateBook);
 router.delete("/deletebook/:id", deleteBook);
 
 export default router;  
